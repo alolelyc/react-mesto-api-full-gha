68008f03b952a9c70ddcf219d52ee8aca22dccae
Olga Elyakova <alolelyc@yandex.ru>
2023-06-03 18:36:22 +0300

    ПР15 изменение порта

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const {
   validationCreateUser,
 } = require('./middlewares/validators/userValidation');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3002 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
